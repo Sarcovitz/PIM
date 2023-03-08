c37a3435865ad0835dec5651b58ec9fa51271b9a
Filip <fsarkowicz@gmail.com>
2023-03-08 18:49:38 +0100

    zmiany pod create product oage

--- PimModels/RequestModels/CreateCatalog.cs
+++ PimModels/RequestModels/CreateCatalog.cs
@@ -5,7 +5,7 @@ namespace PimModels.RequestModels;
 public class CreateCatalog
 {
     [Required]
-    [RegularExpression(@"[A-Za-z0-9]{3,20}", ErrorMessage = "Catalog name can contain only letters and numbers.")]
+    [RegularExpression(@"[A-Za-z0-9\s]{3,20}", ErrorMessage = "Catalog name can contain only letters and numbers.")]
     [StringLength(20, MinimumLength = 3, ErrorMessage = "Catalog name length must be between 3 and 20 characters long.")]
     public string Name { get; set; }
     [Required]
